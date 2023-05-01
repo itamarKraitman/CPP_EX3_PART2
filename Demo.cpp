6cd2b2146f8c9366a66351a851701a577db5e525
itamarKraitman <itamar.k96@gmail.com>
2023-05-02 02:41:33 +0300

    update demo

--- Demo.cpp
+++ Demo.cpp
@@ -9,40 +9,29 @@
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
-#include <limits>
-#include <cmath>
-#include <algorithm>
-#include <iomanip>
-
+using namespace std;
 
 #include "sources/Fraction.hpp"
-using namespace std;
 
 using namespace ariel;
 
-int main()
-{
-    // Fraction a(5,3), b(14,21);
-    // cout << "a: " << a << "b: " << b << endl;
-    // cout << "a+b" << a+b << endl;
-    // cout << "a-b" << a-b << endl;
-    // cout << "a/b" << a/b << endl;
-    // cout << "a*b" << a*b << endl;
-    // cout << "2.3*b" << 2.3*b << endl;
-    // cout << "a+2.421" << a+2.421 << endl;
-    // Fraction c = a+b-1;
-    // cout << c++ << endl;
-    // cout << --c << endl;
 
-    // cout << "c >=b ? : " << (c >= b) << endl;
-    // if (a > 1.1) cout << " a is bigger than 1.1" << endl;
-    // else cout << " a is smaller than 1.1" << endl;
+int main() {
+    Fraction a(5,3), b(14,21);
+    cout << "a: " << a << "b: " << b << endl;
+    cout << "a+b" << a+b << endl; 
+    cout << "a-b" << a-b << endl; 
+    cout << "a/b" << a/b << endl; 
+    cout << "a*b" << a*b << endl; 
+    cout << "2.3*b" << 2.3*b << endl; 
+    cout << "a+2.421" << a+2.421 << endl; 
+    Fraction c = a+b-1;
+    cout << c++ << endl;
+    cout << --c << endl;
 
-   Fraction a(-68650,75000);
-   cout << a << endl;
-   float s = 5465.1541561;
-   float ss = std::round(s * 1000.0) / 1000.0;
-   cout << ss << endl; 
+    cout << "c >=b ? : " << (c >= b) << endl;
+    if (a > 1.1) cout << " a is bigger than 1.1" << endl;
+    else cout << " a is smaller than 1.1" << endl;
 
-    
+
 }
